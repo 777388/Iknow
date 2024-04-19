80cbf1eebc6f752038d666b04593c4f450dda6fa
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-04-19 02:26:02 -0700

    Add files via upload

---
+++ iknow.py
@@ -0,0 +1,9 @@
+import keyword
+class human:
+    def __init__(I):
+        know = keyword.iskeyword(round)
+        if (I == know):
+            try:
+                return True
+            except:
+                return True
